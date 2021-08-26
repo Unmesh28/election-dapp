2e2f2beeb4390c58d2f543f6f6e325e39a5df894
Unmesh Dabhade <unmesh.dabhade@gmail.com>
2021-08-26 11:28:42 +0530

    Multiple candidates added

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,10 +2,26 @@ pragma solidity ^0.8;
 
 contract Election {
 
-    string public candidate;
+    //Model candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    //fetch candidates
+    mapping(uint => Candidate) public candidates;
+    //store candidates count
+    uint public candidatesCount;
 
     //Constructor
-    constructor() { 
-        candidate =  "Candidate 1";
+    constructor() {  
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
     }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] =  Candidate(candidatesCount, _name, 0);
+    } 
 }
